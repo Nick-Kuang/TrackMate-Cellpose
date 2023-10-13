ec1f35bc3454627897754dd6523151782d28cf0f
Nick Kuang <111142664+Nick-Kuang@users.noreply.github.com>
2023-10-13 10:03:26 -0400

    Prelim Lacss.py file

---
+++ src/main/java/fiji/plugin/trackmate/lacss/lacss.py
@@ -0,0 +1,19 @@
+import lacss
+import imageio.v2 as imageio
+
+path = #dir of files
+
+model_path = #cp path to model
+
+
+predictor = Predictor(cp_path)
+
+for file in path:
+    image = image.imread(file)
+    img = rescale(image.astype("float32"), 2)
+    img -= img.mean()
+    img /= img.std()
+    img = img[..., None]
+
+    #pred = predictor.predict(img)
+    pred_label = predictor.predict_label(img)
